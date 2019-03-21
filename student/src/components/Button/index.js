20e2b3c20bcb7a2a20b4d0bf0899fc4a99e5af7c
razantayyem <razanr543@gmail.com>
2019-03-21 21:17:04 +0200

    Create Button component and styled it #40

---
+++ student/src/components/Button/index.js
@@ -0,0 +1,17 @@
+import React,{Component} from "react";
+
+import StyledButton from "./index.style";
+
+class Button extends Component {
+  constructor(props){
+    super(props);
+  }
+    render () {
+      return(
+        <StyledButton>
+          {this.props.value}
+        </StyledButton>
+      )
+    }
+}
+export default Button;
---
+++ student/src/components/Button/index.style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default styled.button`
+background: #38BEFD;
+border-radius: 20px;
+position: absolute;
+width: 198px;
+height: 44px;
+left: 89px;
+top: 120px;
+color:white;
+`;
