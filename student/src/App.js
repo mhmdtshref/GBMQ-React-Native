938b4ee35311ca478c083ec7bf16fff252d01296
razantayyem <razanr543@gmail.com>
2019-03-24 15:10:50 +0200

    Update the test of Form component #39

--- student/src/App.js
+++ student/src/App.js
@@ -1,34 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import MainForm from './components/Form';
 
 class App extends Component {
-
-onSubmitAction = () => {
-  return 0;
-}
-
-
   render() {
-    const initialValues = { name: '', password: '' };
-    const fields = [
-      {
-        type: "text",
-        name: "name",
-        placeholder: "Name",
-      },
-      {
-         type: "text",
-         name: "password",
-         placeholder: "Password",
-       },
-
-
-
-    ];
     return (
       <div className="App">
-      <MainForm fields={fields} action={this.submitAction} operationName="Login" initialValues={initialValues} />
       </div>
     );
   }
