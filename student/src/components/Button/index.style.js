386da2e31ce332a8dc3154e981a2d42b2ea66cf6
razantayyem <razanr543@gmail.com>
2019-03-24 14:55:55 +0200

    Update style #40

--- student/src/components/Button/index.style.js
+++ student/src/components/Button/index.style.js
@@ -3,11 +3,9 @@ import styled from "styled-components";
 export default styled.button`
 background: #38BEFD;
 border-radius: 20px;
-position: absolute;
 width: 198px;
 height: 44px;
-left: 89px;
-top: 120px;
 color:white;
 border:none;
+margin-top:5px;
 `;
