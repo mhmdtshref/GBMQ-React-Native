84944514027320f7465ce1db7562930dd95b5ca1
mhmdtshref <mhmdtshref@gmail.com>
2019-03-25 15:29:59 +0200

    Initialize Quiz component #27

---
+++ student/src/components/Quiz/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Quiz extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+
+        };
+    };
+
+    render(){
+        return <div>Hello world!</div>
+    };
+
+}
+
+export default Quiz;
