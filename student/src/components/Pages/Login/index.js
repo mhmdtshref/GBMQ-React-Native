e6685a421ef8ba5df38363927081ac658e3f826f
razantayyem <razanr543@gmail.com>
2019-03-26 10:13:07 +0200

    Create the design for login page #23

--- student/src/components/Pages/Login/index.js
+++ student/src/components/Pages/Login/index.js
@@ -1,13 +1,47 @@
-import React, { Component } from "react";
+
+
+import React, { Component } from 'react';
+import MainForm from '../../Form';
+import {Title, Title1, StyledPage, StyledBk, StyledSpan, StyledLink, StyledDiv} from "./index.style";
+import bg from './login-bg.png';
+
 
 
 class Login extends Component {
+
+onSubmitAction = () => {
+  return 0;
+}
+
   render() {
+    const initialValues = { name: '', password: '' };
+    const fields = [
+      {
+        type: "text",
+        name: "name",
+        placeholder: "Name",
+      },
+      {
+         type: "text",
+         name: "password",
+         placeholder: "Password",
+       },
+    ];
     return (
-      <div>
-        <h1 className='title'> GBMQ </h1>
-      </div>
+      <React.Fragment>
+      <StyledPage>
+      <StyledBk src={bg}/>
+      <Title>Great British Money</Title>
+      <Title1>Quiz</Title1>
+      <MainForm fields={fields} action={this.onSubmitAction} operationName="Login" initialValues={initialValues} />
+      <StyledDiv>
+      <StyledSpan>New User ?</StyledSpan>
+      <StyledLink> Signup </StyledLink>
+      </StyledDiv>
+      </StyledPage>
+      </React.Fragment>
     );
   }
 }
+
 export default Login;
