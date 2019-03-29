eb8d2825c63f6fc7028ce9a8b82f0240a456b307
mhmdtshref <mhmdtshref@gmail.com>
2019-03-29 22:06:27 +0300

    Modify popup activity page #28

---
+++ student/src/components/Pages/Activities/index.style.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const ActivitiesGroup = styled.div`
+    background-color: #38BEFD;
+    width: 90vw;
+    height: 60vh;
+    margin: 30vw 5vw 10vh 5vw;
+    padding-top: 10vh;
+    position: absolute;
+    top: 0;
+`;
+export const ActivityButton = styled.button`
+    background-color: #F2F5FA;
+    border-radius: 25px;
+    border: none;
+    width: 70%;
+    margin: 5% 15% 5% 15%;
+    height: 5vh;
+`;
+
+export const NextActivityButton = styled.button`
+    background-color: #F2F5FA;
+    border-radius: 25px;
+    border: none;
+    width: 70%;
+    margin: 5% 15% 5% 15%;
+    height: 5vh;
+    color: gray;
+`;
