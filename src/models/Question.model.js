fd02125a948c3c43c714c1ecdd27c6573815d947
mhmdtshref <mhmdtshref@gmail.com>
2019-03-31 11:51:38 +0300

    eslint update #60

--- src/models/Question.model.js
+++ src/models/Question.model.js
@@ -2,10 +2,10 @@ const Sequelize = require('sequelize');
 const sequelize = require('./sequelize.js');
 
 const Question = sequelize.define('questions', {
-    text: { type: Sequelize.STRING(255), allowNull: false },
-    imageUrl: { type: Sequelize.STRING(512), allowNull: false },
-    type: { type: Sequelize.BOOLEAN, allowNull: false },
-    quizNo: { type: Sequelize.INTEGER, allowNull: false },
+  text: { type: Sequelize.STRING(255), allowNull: false },
+  imageUrl: { type: Sequelize.STRING(512), allowNull: false },
+  type: { type: Sequelize.BOOLEAN, allowNull: false },
+  quizNo: { type: Sequelize.INTEGER, allowNull: false },
 });
 
-module.exports  = Question;
+module.exports = Question;
