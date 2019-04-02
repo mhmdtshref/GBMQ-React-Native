06e1fce28585c74de99686ed83e25408d266313f
mhmdtshref <mhmdtshref@gmail.com>
2019-04-02 15:58:44 +0300

    use cookie parser in the app #77

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const router = require('./router');
+const cookieParser = require('cookie-parser');
 require('env2')('config.env');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'student', 'build')));
 // TODO: Run the admin app using:
 // app.use('/admin', express.static(path.join(__dirname, '..', 'admin', 'build')));
