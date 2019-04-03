77d818db4e948ff4ee1a556261db4330c654f342
mhmdtshref <mhmdtshref@gmail.com>
2019-04-03 11:07:49 +0300

    Modify server to handle the views #60

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
-const { sequelize } = require('./models/');
+const { sequelize, Quiz1Mark, Quiz2Mark } = require('./models/');
 const app = require('./app');
 require('env2')('config.env');
 
 app.set('PORT', process.env.PORT);
-sequelize.sync().then(() => {
-  app.listen(app.get('PORT'), () => {
-    // eslint-disable-next-line no-console
-    console.log(`The Server Is Up And Running On http://localhost:${app.get('PORT')}`);
-  });
-});
+Quiz2Mark.drop()
+    .then(() => {
+      Quiz1Mark.drop().then(() => {
+          sequelize.sync().then(() => {
+              app.listen(app.get('PORT'), () => {
+                  // eslint-disable-next-line no-console
+                  console.log(`The Server Is Up And Running On http://localhost:${app.get('PORT')}`);
+              });
+          });
+      })
+    });
