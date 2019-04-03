4be8b66bad91e84fd7f5f9d855a10699b2095a58
mhmdtshref <mhmdtshref@gmail.com>
2019-04-03 13:47:25 +0300

    eslint fixing #77

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-const { sequelize, Quiz1Mark, Quiz2Mark } = require('./models/');
+const { sequelize } = require('./models/');
 const app = require('./app');
 require('env2')('config.env');
 
 app.set('PORT', process.env.PORT);
 sequelize.sync().then(() => {
-app.listen(app.get('PORT'), () => {
+  app.listen(app.get('PORT'), () => {
   // eslint-disable-next-line no-console
-  console.log(`The Server Is Up And Running On http://localhost:${app.get('PORT')}`);
-});
+    console.log(`The Server Is Up And Running On http://localhost:${app.get('PORT')}`);
+  });
 });
