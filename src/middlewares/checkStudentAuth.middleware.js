4cb02e8c8ef5132ec388f4a3dcb4fae932ce8835
mhmdtshref <mhmdtshref@gmail.com>
2019-04-03 13:08:58 +0300

    Create student authentication middleware

--- src/middlewares/checkStudentAuth.middleware.js
+++ src/middlewares/checkStudentAuth.middleware.js
@@ -5,13 +5,12 @@ require('env2')('config.env');
 const { SECRET } = process.env;
 
 const checkStudentAuth = (req, res, next) => {
-    console.log("REQUEST IS: ", req);
     const hashedId = req.cookies.id;
 
     if(!hashedId){
         res.json({
-            success: true, data: {
-            studentState: 0,
+            success: false, data: {
+            error: 'Unauthenticated',
             },
         });
     } else {
