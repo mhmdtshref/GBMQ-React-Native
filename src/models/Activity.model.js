6fc4bb572e89adf5dd335c02b2b3788f3be0218b
mhmdtshref <mhmdtshref@gmail.com>
2019-04-04 09:12:33 +0300

    update table name #89

--- src/models/Activity.model.js
+++ src/models/Activity.model.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize-views-support');
 const sequelize = require('./sequelize.js');
 
-const Activity = sequelize.define('admins', {
+const Activity = sequelize.define('activities', {
     key: { type: Sequelize.STRING(255), allowNull: false },
     link: { type: Sequelize.STRING(255), allowNull: false },
     description: { type: Sequelize.STRING(255), allowNull: true },
