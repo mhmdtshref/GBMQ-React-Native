5cdbf9eb312bc87806aeebfa783d184dbcd5eb0a
mhmdtshref <mhmdtshref@gmail.com>
2019-04-04 09:06:45 +0300

    create activity model #89

---
+++ src/models/Activity.model.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize-views-support');
+const sequelize = require('./sequelize.js');
+
+const Activity = sequelize.define('admins', {
+    key: { type: Sequelize.STRING(255), allowNull: false },
+    link: { type: Sequelize.STRING(255), allowNull: false },
+    description: { type: Sequelize.STRING(255), allowNull: true },
+});
+
+module.exports = Activity;
