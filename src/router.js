1e4bf6a830b14e14e70156710684bd27cff4f2d6
mhmdtshref <mhmdtshref@gmail.com>
2019-04-04 13:31:49 +0300

    Add router that get question id as parameter and response with question and choices #93

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ const path = require('path');
 const AuthController = require('./controllers/Auth.controller');
 const AdminController = require('./controllers/Admin.controller.js');
 const StudentController = require('./controllers/Student.controller');
-
+const QuestionController = require('./controllers/Question.controller');
 
 // const validations = require('./validations');
 
@@ -14,6 +14,7 @@ router.post('/login', AuthController.login);
 router.post('/postQuestion', AdminController.postQuestion);
 
 router.get('/checkState', StudentController.checkState);
+router.get('/getQuestion/:questionId', QuestionController.getQuestionById);
 
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'student', 'build', 'index.html'));
