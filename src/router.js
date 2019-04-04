4a04b2e6313a7a4dc3c8a49c3ffcf3f14fb100a1
mhmdtshref <mhmdtshref@gmail.com>
2019-04-04 15:44:00 +0300

    Add new router to post quiz student #95

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ const AuthController = require('./controllers/Auth.controller');
 const AdminController = require('./controllers/Admin.controller.js');
 const StudentController = require('./controllers/Student.controller');
 const QuizController = require('./controllers/Quiz.controller');
-
+const StudentAuthMiddleware = require('./middlewares/checkStudentAuth.middleware');
 // const validations = require('./validations');
 
 const router = express.Router();
@@ -16,6 +16,7 @@ router.post('/postQuestion', AdminController.postQuestion);
 router.get('/checkState', StudentController.checkState);
 
 router.get('/quizQuestionsIds', QuizController.getQuizQuestionsIds);
+router.post('/postQuiz',[StudentAuthMiddleware.checkStudentAuth], QuizController.postQuiz);
 
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'student', 'build', 'index.html'));
