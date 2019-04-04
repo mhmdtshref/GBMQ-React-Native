6131fe1338e8594ecd0e1289c5c985223594375b
mhmdtshref <mhmdtshref@gmail.com>
2019-04-04 13:29:05 +0300

    Create function that get the question with its choices by question id #93

--- src/controllers/Question.controller.js
+++ src/controllers/Question.controller.js
@@ -1,7 +1,7 @@
 const {
   Question,
 } = require('../models');
-
+const ChoiceController = require('./Choice.controller');
 const createQuestion = (question) => {
   const {
     text, imageUrl, type, quizNo,
@@ -11,6 +11,35 @@ const createQuestion = (question) => {
   });
 };
 
+const addQuestionChoices = (question) => new Promise((resolve, reject) => {
+    if(question){
+        ChoiceController.getChoiceByQuestionId(question.id)
+            .then((choices) => {
+                resolve({ question, choices });
+            })
+            .catch((err) => {
+                reject(err);
+            })
+    } else {
+        reject(new Error('No question found'));
+    }
+
+});
+
+
+const getQuestionById = (req, res) => {
+    const {questionId} = req.params;
+    Question.findOne({where: {id: questionId}})
+        .then(addQuestionChoices)
+        .then(({ question, choices }) => {
+            res.json({ success: true, data: { question, choices } });
+        })
+        .catch((err) => {
+            res.json({ success: false, error: err.message });
+        });
+};
+
+
 module.exports = {
-  createQuestion,
+  createQuestion, getQuestionById
 };
