9e51de6bc0f83ba9e3fc309f8baf5ea24f3514b2
mhmdtshref <mhmdtshref@gmail.com>
2019-04-04 12:07:58 +0300

    add router to get question ids #92

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ const path = require('path');
 const AuthController = require('./controllers/Auth.controller');
 const AdminController = require('./controllers/Admin.controller.js');
 const StudentController = require('./controllers/Student.controller');
-
+const QuizController = require('./controllers/Quiz.controller');
 
 // const validations = require('./validations');
 
@@ -14,6 +14,8 @@ router.post('/login', AuthController.login);
 router.post('/postQuestion', AdminController.postQuestion);
 
 router.get('/checkState', StudentController.checkState);
+
+router.get('/quizQuestionsIds', QuizController.getQuizQuestionsIds);
 
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'student', 'build', 'index.html'));
