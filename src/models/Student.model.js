b213989ad0f77f4816daffe0f509adc99bef937c
razantayyem <razanr543@gmail.com>
2019-04-08 23:15:20 +0300

    Make Validation for username in signup page #113

--- src/models/Student.model.js
+++ src/models/Student.model.js
@@ -1,32 +1,70 @@
 const Sequelize = require('sequelize-views-support');
 const sequelize = require('./sequelize.js');
 
+const Student = sequelize.define(
+  'students',
+  {
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        fn(val) {
+          if (val === '') throw new Error('Please insert your username');
+        },
+        isUnique(value, next) {
+          Student.find({
+            where: {
+              username: value,
+            },
+          }).then((result) => {
+            if (result === null) {
+              return next();
+            }
+            return next(' Username already exist');
+          }).catch(err => next(err));
+        },
+      },
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: 6,
+          msg: 'Your password must be at least 6 character',
+        },
+      },
 
-const Student = sequelize.define('students', {
-  username: {
-    type: Sequelize.STRING,
-    unique: true,
-    allowNull: false,
+    },
+    age: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    gender: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+      validate: {
+        isBoolean: true,
+      },
+    },
+    english: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+      validate: {
+        isBoolean: true,
+      },
+    },
+    postcode: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      len: {
+        args: 2,
+        msg: 'Your password must be at least 6 character',
+      },
+    },
   },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  age: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  gender: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  english: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  postcode: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-}, { freezeTableName: true });
+  { freezeTableName: true },
+);
 module.exports = Student;
