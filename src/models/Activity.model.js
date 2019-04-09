8f0fb38d685a97473627391dda45e823a87e2050
mhmdtshref <mhmdtshref@gmail.com>
2019-04-09 11:16:53 +0300

    Add new column (day) to the activity table model #127

--- src/models/Activity.model.js
+++ src/models/Activity.model.js
@@ -3,6 +3,7 @@ const sequelize = require('./sequelize.js');
 
 const Activity = sequelize.define('activities', {
     key: { type: Sequelize.STRING(255), allowNull: false },
+    day: { type: Sequelize.INTEGER, allowNull: false, unique: true },
     link: { type: Sequelize.STRING(255), allowNull: false },
     description: { type: Sequelize.STRING(255), allowNull: true },
 });
