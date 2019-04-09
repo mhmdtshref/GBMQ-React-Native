6b8ef970e13afb70e6d93f7665baf72673821476
Amin <ayman201266@hotmail.com>
2019-04-09 14:54:38 +0300

    fix eslint errors

--- src/controllers/Quiz.controller.js
+++ src/controllers/Quiz.controller.js
@@ -2,35 +2,30 @@ const Question = require('../models/Question.model');
 const StudentChoice = require('../models/StudentsChoice.model');
 
 const getQuizQuestionsIds = (req, res) => {
-    const { quizId } = req.params;
-    Question.findAll({ where: { quizNo: quizId }, attributes: [ 'id' ] })
-        .then((questions) => {
-            return questions.map((question) => question.id);
-        }).then((ids) => {
-            res.json({ success: true, data: { quizQuestionsIds: ids } });
+  const { quizId } = req.params;
+  Question.findAll({ where: { quizNo: quizId }, attributes: ['id'] })
+    .then(questions => questions.map(question => question.id)).then((ids) => {
+      res.json({ success: true, data: { quizQuestionsIds: ids } });
     })
-        .catch((err) => {
-            res.json({ success: false, error: err.message });
-        })
+    .catch((err) => {
+      res.json({ success: false, error: err.message });
+    });
 };
 
 const postQuiz = (req, res) => {
-    const { choices } = req.body;
-    const choicesPromises = (choices).map((choiceId) => {
-        return StudentChoice.create({ choiceId, studentId: req.studentId });
+  const { choices } = req.body;
+  const choicesPromises = (choices).map(choiceId => StudentChoice.create({ choiceId, studentId: req.studentId }));
+  Promise.all(choicesPromises)
+    .then(() => {
+      res.json({ success: true });
+    })
+    .catch((err) => {
+      res.json({ success: false, error: err.message });
     });
-    Promise.all(choicesPromises)
-        .then(() => {
-            res.json({ success: true });
-        })
-        .catch((err) => {
-            res.json({ success: false, error: err.message });
-        });
 };
 
 
-
 module.exports = {
-    getQuizQuestionsIds,
-    postQuiz
+  getQuizQuestionsIds,
+  postQuiz,
 };
