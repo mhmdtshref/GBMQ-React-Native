a9dd5b60b9e0985759cdc2bbcba07e83505cf660
mhmdtshref <mhmdtshref@gmail.com>
2019-04-10 13:35:04 +0300

    Make front-end waiting for the remaining days to ge tquiz 2 and show the resuklt in the disabled button #136

--- student/src/components/Pages/Home/index.js
+++ student/src/components/Pages/Home/index.js
@@ -21,7 +21,6 @@ class Home extends Component {
   constructor(props){
      super(props);
      this.state = {
-       Quiz2Enabled: true,
        activitiesPopup: false,
    };
  }
@@ -32,7 +31,7 @@ class Home extends Component {
          .get('/checkState')
          .then(({data})=>{
              if (data.success && data.data.studentState >= 0) {
-                     this.setState({ studentState: data.data.studentState }, () => {
+                     this.setState({ studentState: data.data.studentState, remainingDays: data.data.remainingDays }, () => {
                          resolve(data.data.studentState);
                      });
              } else {
@@ -44,25 +43,42 @@ class Home extends Component {
          })
  });
 
- componentDidMount() {
-     if(this.state.studentState !== 2){
-         this.checkStudentState()
-             .then((studentState) => {
-                 const {history} = this.props;
-                 switch (studentState) {
-                     case 0:
-                         history.push('/login');
-                         break;
-                     case 1:
-                         history.push('/start');
-                         break;
-                     case 3:
-                         history.push('/Comparison');
-                         break;
-                 }
-             })
-     }
- }
+  redirectByState = () => new Promise((resolve) => {
+      if(this.state.studentState !== 2){
+          this.checkStudentState()
+              .then((studentState) => {
+                  const {history} = this.props;
+                  switch (studentState) {
+                      case 0:
+                          history.push('/login');
+                          break;
+                      case 1:
+                          history.push('/start');
+                          break;
+                      case 2:
+                          resolve();
+                          break;
+                      case 3:
+                          history.push('/Comparison');
+                          break;
+                  }
+              })
+      }
+  });
+
+  renderQuiz2Button = () => {
+      const { history } = this.props;
+      console.log("I'm HERE!");
+      this.setState((prevState) => ({ Quiz2Button: prevState.remainingDays <= 0 ?
+              <StyledQuizButton onClick = {() => {this.props.onStartAction(2,history);}} > Go to Second Quiz </StyledQuizButton>
+              : <StyledQuizButton disabled={true}> Days remaining: {this.state.remainingDays} </StyledQuizButton> }));
+  };
+
+  componentDidMount() {
+      this.redirectByState()
+          .then(this.renderQuiz2Button)
+          .catch((err) => { alert(`Loading Error: ${err.message}`) })
+  }
 
   openActivities = () => {
       this.setState({ activitiesPopup: true });
@@ -75,8 +91,6 @@ class Home extends Component {
     };
 
   render() {
-    const { history } = this.props;
-    console.log(this.props);
     if (this.state.studentState !== 2) {
         return <StyledLoading> Loading... </StyledLoading>
         }
@@ -91,7 +105,7 @@ class Home extends Component {
                 <MessageText> Now, you need to view tutorials videos click on the button below </MessageText>
                 <StyledImg src={homeImg} />
                 <StyledActivitiesButton onClick = {this.openActivities}> Watch Course Videos </StyledActivitiesButton>
-                <StyledQuizButton onClick = {() => {this.props.onStartAction(2,history);}} > Go to Second Quiz </StyledQuizButton>
+                {this.state.Quiz2Button}
             </StyledHome>
             { this.state.activitiesPopup ? <Activities/> : null}
         </StyledContent>
