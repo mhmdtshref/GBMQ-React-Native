1e9c92455bcb17e7ee6244bed574fc689dc2ad34
mhmdtshref <mhmdtshref@gmail.com>
2019-04-11 11:48:43 +0300

    Update form margins #122

--- student/src/components/Form/index.style.js
+++ student/src/components/Form/index.style.js
@@ -8,7 +8,7 @@ export const StyledForm = styled(Form)`
   flex-flow: column wrap;
   align-items: center;
   flex-wrap: wrap;
-  margin: 10vh 15vh 0vh 15vw
+  margin: 10vh 15vw 0vh 15vw
   width: 70%;
   position: relative;
 `;
