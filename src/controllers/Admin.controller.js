a7154bf8cd0817ed5b0b8518b6d372a4c2eb57a7
mhmdtshref <mhmdtshref@gmail.com>
2019-04-15 10:24:44 +0300

    Update excel sheet to be readable #149

--- src/controllers/Admin.controller.js
+++ src/controllers/Admin.controller.js
@@ -1,4 +1,5 @@
 const { Question, Choice, StudentsView } = require('../models');
+const excel = require('exceljs');
 
 const createQuestion = (question) => {
   const {
@@ -39,9 +40,24 @@ const postQuestion = (req, res) => {
 };
 
 const getStatisticsFile = (req, res) => {
-    StudentsView.findAll({ attributes: ['username', 'age', 'gender', 'postcode', 'english'] })
+    StudentsView.findAll({ attributes: ['username', 'age', 'gender', 'postcode', 'english', 'q1mark', 'q2mark'] })
         .then((students) => {
-            console.log(students.map(student => student.dataValues));
+            const workbook = new excel.Workbook();
+            const sheet = workbook.addWorksheet('students-data');
+            sheet.addRow(Object.keys(students[0].dataValues));
+            students.forEach((student) => {
+                console.log(student.dataValues);
+                sheet.addRow().values = Object.values({ ...student.dataValues, gender: (student.dataValues.gender ? 'Male' : 'Female'), english: (student.dataValues.english ? 'Yes' : 'No') });
+            });
+            res.set({
+                'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                'Content-Disposition': 'attachment; filename=logs.xlsx',
+            });
+            workbook.xlsx.write(res).then(() => {
+                res.end();
+            });
+
+            //console.log(students.map(student => student.dataValues));
         })
 };
 
