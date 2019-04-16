bece77d725c1200de2b6dfd5cb8090e7f7f8e67d
mhmdtshref <mhmdtshref@gmail.com>
2019-04-16 10:42:42 +0300

    Create field component #156

---
+++ admin/src/components/Field/index.style.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+
+export const StyledDiv = styled.div`
+    width: 100%;
+`;
+
+export const StyledLabel = styled.label`
+    width: 100%;
+    font-family: 'ABeeZee', sans-serif;
+    font-size: 1em;
+    color: #767A7D;
+    margin:5vh 12vw 3vh 0vw;
+`;
+
+export const StyledInput = styled.input`
+    width: 100%;
+    border-radius: 50px;
+    height: 5vh;
+    border: 2px solid #08a3c3;
+    padding-left: 4%;
+    font-size: 90%;
+    margin: 1.5vh 0 1.5vh 0;
+`;
