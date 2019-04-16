eac7c7e8005916fbf0d1d578c0ade75560875468
Amin <ayman201266@hotmail.com>
2019-04-16 14:24:38 +0300

    Download students sheets #166

---
+++ admin/src/components/StudentSheets/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import {
+  Title,
+  StyledButton
+} from "./index.style";
+
+class StdSheets extends Component {
+
+
+
+  downloadSheets = () => {
+    window.open("/getStatisticsFile", "_blank");
+  }
+
+  render() {
+        return <React.Fragment>
+          <Title>GBMQ Students Sheets</Title>
+          <StyledButton onClick={this.downloadSheets}>Download Sheets</StyledButton>
+        </React.Fragment>
+    }
+}
+
+export default StdSheets;
