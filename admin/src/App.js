7b71dac3782abe9d96cb1aca85f2197b0aae0d0a
mhmdtshref <mhmdtshref@gmail.com>
2019-04-16 10:52:47 +0300

    Update App.js to be without react router dom #156

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Route, BrowserRouter as Router} from 'react-router-dom';
 import AdminLogin from './components/AdminLogin';
 
 
 class App extends Component {
   render() {
-    return (
-      <Router>
-      <div className="App">
-        <Route path='/adminLogin' component={AdminLogin}/>
-      </div>
-      </Router>
-    );
+    return <AdminLogin/>
   }
 }
 
