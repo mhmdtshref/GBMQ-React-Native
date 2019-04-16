af056fc0c1ed0aebfe7de5cb060afb1c32a6fb21
mhmdtshref <mhmdtshref@gmail.com>
2019-04-16 15:09:35 +0300

    Update app.js to check login state and display correct page #165

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import { StyledLoading } from './App.style.js';
 import AdminLogin from './components/AdminLogin';
-
+import DownloadSheet from './components/StudentSheets';
+import axios from 'axios';
 
 class App extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            loginState: null,
+            loginState: 0,
         };
     }
 
@@ -16,8 +18,27 @@ class App extends Component {
         this.setState({ loginState }, console.log("State:: ", this.state));
     };
 
+    componentDidMount(){
+        axios.get('/admin/checkAuth')
+            .then(({ data }) => {
+                if(!data.success){
+                    this.setLoginState(1);
+                } else {
+                    this.setLoginState(2);
+                }
+            });
+    };
+
     render() {
-        return <AdminLogin setLoginState={this.setLoginState} />;
+        switch (this.state.loginState) {
+            case 0:
+                return <StyledLoading>Loading...</StyledLoading>;
+            case 1:
+                return <AdminLogin setLoginState={this.setLoginState}/>;
+            case 2:
+            default:
+                return <DownloadSheet/>;
+        }
     }
 }
 
